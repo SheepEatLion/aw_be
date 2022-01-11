a6506a2740593037e6efab394c15c24e64eb4ef0
SheepEatLion <gowjr207@gmail.com>
2022-01-11 23:00:41 +0900

    Initial commit

---
+++ src/main/java/com/mynameisjunyeong/aw_be/AwBeApplication.java
@@ -0,0 +1,13 @@
+package com.mynameisjunyeong.aw_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mynameisjunyeong/aw_be/AwBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mynameisjunyeong.aw_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
