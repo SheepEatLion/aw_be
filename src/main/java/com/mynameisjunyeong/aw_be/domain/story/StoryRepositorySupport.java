2edabdf554e7f2f643e298f2bb6d9727f7fb2fee
SheepEatLion <gowjr207@gmail.com>
2022-01-17 23:54:36 +0900

    refactor: querydsl 설정변경
    - 설정 클래스 생성하고 dao 클래스에 주입

--- src/main/java/com/mynameisjunyeong/aw_be/domain/story/StoryRepositorySupport.java
+++ src/main/java/com/mynameisjunyeong/aw_be/domain/story/StoryRepositorySupport.java
@@ -1,10 +1,18 @@
 package com.mynameisjunyeong.aw_be.domain.story;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class StoryRepositorySupport extends QuerydslRepositorySupport {
 
-    public StoryRepositorySupport(){
+    @Autowired
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public StoryRepositorySupport(JPAQueryFactory jpaQueryFactory){
         super(Story.class);
+        this.jpaQueryFactory = jpaQueryFactory;
     }
 }
