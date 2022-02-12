0fa896749ff574c5e77e2de71c24fd109f57f853
SheepEatLion <gowjr207@gmail.com>
2022-02-12 18:53:46 +0900

    feat : 개발환경에서 시큐리티 허용설정

--- src/main/java/com/mynameisjunyeong/aw_be/config/SecurityConfig.java
+++ src/main/java/com/mynameisjunyeong/aw_be/config/SecurityConfig.java
@@ -7,25 +7,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-/*
+
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthTokenProvider authTokenProvider;  // kakao
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        super.configure(web);
-    }
+    //private final AuthTokenProvider authTokenProvider;  // kakao
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authTokenProvider);
+        //JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authTokenProvider);
 
-        http.authorizeHttpRequests()
+        http.authorizeHttpRequests().mvcMatchers("/").permitAll();
     }
 
 
 }
-*/
