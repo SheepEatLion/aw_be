ebd5edd0c2290a2c5c28025b56745a13222a21df
SheepEatLion <gowjr207@gmail.com>
2022-02-22 17:59:18 +0900

    fix: user entity
    - 컬럼 변경

--- src/main/java/com/mynameisjunyeong/aw_be/api/domain/user/User.java
+++ src/main/java/com/mynameisjunyeong/aw_be/api/domain/user/User.java
@@ -15,7 +15,7 @@ import javax.validation.constraints.Size;
  * @author junyeong
  */
 
-@Getter @Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -27,49 +27,38 @@ public class User extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userSeq;
 
-    @Column(name = "USER_ID", nullable = false, unique = true)
-    private String userId;
-
     @Column(length = 100, nullable = false, unique = true)
     private String email;
 
     @Column(length = 100, nullable = false)
-    private String password;
-
-    @Column(length = 100, nullable = false)
-    private String username;
+    private String name;
 
     @Column(name = "ROLE_TYPE", length = 20, nullable = false)
     @Enumerated(EnumType.STRING)
     private RoleType roleType;
 
-    @Column(name = "PROVIDER_TYPE", length = 20, nullable = false)
-    @Enumerated(EnumType.STRING)
-    private ProviderType providerType;
-
-    @Column(name = "EMAIL_VERIFIED_YN", length = 1, nullable = false)
-    private String emailVerifiedYn;
-
-    @Column(name = "PROFILE_IMAGE_URL", length = 512, nullable = false)
-    private String profileImageUrl;
+    @Column(length = 512)
+    private String picture;
 
     // create at
     // modified at
 
-    public User(
-            @NotNull @Size(max = 64) String userId, @NotNull @Size(max = 100) String username,
-            @NotNull @Size(max = 512) String email, @NotNull @Size(max = 1) String emailVerifiedYn,
-            @NotNull @Size(max = 512) String profileImageUrl, @NotNull ProviderType providerType,
-            @NotNull RoleType roleType
-    ) {
-        this.userId = userId;
-        this.username = username;
-        this.email = email != null ? email : "NO_EMAIL";
-        this.password = "NO_PASS";
-        this.emailVerifiedYn = emailVerifiedYn;
-        this.profileImageUrl = profileImageUrl != null ? profileImageUrl : "";
-        this.providerType = providerType;
+    @Builder
+    public User(String name, String email, String picture, RoleType roleType) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
         this.roleType = roleType;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.roleType.getKey();
     }
 
 }
