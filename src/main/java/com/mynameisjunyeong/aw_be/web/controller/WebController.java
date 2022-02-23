2b4a156b154c8bdd2ae7f1a827f2d27670a48e14
SheepEatLion <gowjr207@gmail.com>
2022-02-23 19:15:50 +0900

    feat: web controller
    - 인덱스 url 로그인으로 변경

--- src/main/java/com/mynameisjunyeong/aw_be/web/controller/WebController.java
+++ src/main/java/com/mynameisjunyeong/aw_be/web/controller/WebController.java
@@ -1,18 +1,29 @@
 package com.mynameisjunyeong.aw_be.web.controller;
 
+import com.mynameisjunyeong.aw_be.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
 @RequestMapping("/anonymous-writer")
 @Controller
 public class WebController {
 
+    private final HttpSession httpSession;
+
     @GetMapping("/")
     public String index(Model model){
-        model.addAttribute("msg", "i blame on you");
-        return "/index";
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+        return "/login";
     }
 
     @GetMapping("/calendar")
