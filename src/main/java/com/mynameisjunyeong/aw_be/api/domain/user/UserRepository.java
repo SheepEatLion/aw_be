0e751fca7dc2181e47a5355c2e3aa0f30cde9c95
SheepEatLion <gowjr207@gmail.com>
2022-02-25 18:31:42 +0900

    fix: user-repository
    - 안쓰는 메서드 제거

--- src/main/java/com/mynameisjunyeong/aw_be/api/domain/user/UserRepository.java
+++ src/main/java/com/mynameisjunyeong/aw_be/api/domain/user/UserRepository.java
@@ -7,6 +7,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUserId(String userId);
     Optional<User> findByEmail(String email);
 }
